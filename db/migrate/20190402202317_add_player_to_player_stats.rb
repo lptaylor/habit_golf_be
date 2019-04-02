0de18ed208dbd17c8b09710f73e697c79f1eb3cf
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-02 14:26:46 -0600

    Fixes migration issue with table name not being called.

--- db/migrate/20190402202317_add_player_to_player_stats.rb
+++ db/migrate/20190402202317_add_player_to_player_stats.rb
@@ -1,6 +1,6 @@
 class AddPlayerToPlayerStats < ActiveRecord::Migration[5.2]
   def change
-    change_table do |t|
+    change_table :player_stats do |t|
       t.references :players, foreign_key: true
     end
   end
