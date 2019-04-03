b29b8c5fae90be9c15545ca3b37a7f95cc181159
Nick Lindeberg <lindebergnick@gmail.com>
2019-04-03 14:00:32 -0600

    Adds seed files for player, player_stats, clubs and shots Also updates column name on clubs table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,25 @@
 
 player = Player.create(name: "Shooter McGavin", email: "shooter@gmail.com", password:"password")
 player.player_stats.create(hook_percentage: 19.1, pull_percentage: 27.0, great_shot_percentage: 10.3, push_percentage: 23.6, slice_percentage: 20.0)
+
+wedge = Club.create(style_of_club: 1)
+driver = Club.create(style_of_club: 2)
+iron = Club.create(style_of_club: 3)
+
+shot_1 = wedge.shots.create(rating: 1)
+shot_2 = wedge.shots.create(rating: 2)
+shot_3 = wedge.shots.create(rating: 2)
+shot_4 = wedge.shots.create(rating: 3)
+shot_5 = wedge.shots.create(rating: 4)
+
+shot_6 = driver.shots.create(rating: 2)
+shot_7 = driver.shots.create(rating: 1)
+shot_8 = driver.shots.create(rating: 5)
+shot_9 = driver.shots.create(rating: 5)
+shot_10 = driver.shots.create(rating: 5)
+
+shot_11 = iron.shots.create(rating: 4)
+shot_12 = iron.shots.create(rating: 5)
+shot_13 = iron.shots.create(rating: 5)
+shot_14 = iron.shots.create(rating: 5)
+shot_15 = iron.shots.create(rating: 1)
