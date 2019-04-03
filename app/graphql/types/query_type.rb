5e7905eac25784799af33580852ae25c49cc0527
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-03 05:56:42 -0600

    Adds PlayerType and query resolver for all players. IMPORTANT REMOVE PASSWORD AND EMAIL EXPOSURE AFTER BASIC SETUP OF ALL RELATIONSHIPS!

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,10 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_players, [PlayerType], null: false
+
+    def all_players
+      Player.all
     end
   end
 end
