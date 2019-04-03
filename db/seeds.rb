73737b7ce7a101b2c3e76ce919aec0f13c4aeaad
Nick Lindeberg <lindebergnick@gmail.com>
2019-04-03 13:35:22 -0600

    Creates seed for a player and stats for that player.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+player = Player.create(name: "Shooter McGavin", email: "shooter@gmail.com", password:"password")
+player.player_stats.create(hook_percentage: 19.1, pull_percentage: 27.0, great_shot_percentage: 10.3, push_percentage: 23.6, slice_percentage: 20.0)
