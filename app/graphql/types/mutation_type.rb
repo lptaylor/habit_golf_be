ce04071c192bc4f81a56a60e903e4bd41233f747
Nick Lindeberg <lindebergnick@gmail.com>
2019-04-04 12:42:33 -0600

    Adds delete shot mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_player, mutation: Mutations::CreatePlayer
     field :create_shot, mutation: Mutations::CreateShot
+    field :delete_shot, mutation: Mutations::DeleteShot
   end
 end
