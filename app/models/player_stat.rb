81671df5b3d17ec648944f242f58fc3d0e43a2d0
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-05 20:24:28 -0600

    Adds work on the call for the stats database to update on the creation of a new shot.

--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -1,3 +1,29 @@
 class PlayerStat < ApplicationRecord
   belongs_to :player
+
+
+  def self.stats(rating, player_id)
+    (Shot.where("shots.player_id = #{player_id} AND shots.rating = #{rating}")).count / (Shot.where(player_id: player_id)).count * 100
+  end
+
+  def self.determine_shot_type(rating)
+    case rating
+    when 1
+      "hook_percentage"
+    when 2
+      "pull_percentage"
+    when 3
+      "great_shot_percentage"
+    when 4
+      "push_percentage"
+    when 5
+      "slice_percentage"
+    end
+  end
+
+  def self.update_stats(rating, player_id)
+    shot_type = determine_shot_type(rating)
+    where(player_id: player_id).first.update("#{shot_type}": "#{stats(rating,player_id)}")
+    # .("UPDATE player_stats SET #{shot_type} = #{stats(rating, player_id)}")
+  end
 end
--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,4 +1,5 @@
 class Shot < ApplicationRecord
+  after_save PlayerStat.update_stats(self.last.player_id, self.last.rating)
   belongs_to :club
   belongs_to :player
 end
