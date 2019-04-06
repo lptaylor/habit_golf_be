5dd42702c6cb3cc29db4b50ef7a14c2b848f255c
Nick Lindeberg <lindebergnick@gmail.com>
2019-04-06 15:59:50 -0600

    Adds methods to update db on shot post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_133316) do
+ActiveRecord::Schema.define(version: 2019_04_06_195549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_04_05_133316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "player_id"
+    t.float "duff_percentage"
     t.index ["player_id"], name: "index_player_stats_on_player_id"
   end
 
--- spec/models/player_stat_spec.rb
+++ spec/models/player_stat_spec.rb
@@ -6,43 +6,52 @@ RSpec.describe PlayerStat, type: :model do
   end
 
   describe('methods') do
-    it '#determine_shot_type' do
-      shot_type = PlayerStat.determine_shot_type(1)
-      expect(shot_type).to eq("hook_percentage")
-      shot_type = PlayerStat.determine_shot_type(2)
-      expect(shot_type).to eq("pull_percentage")
-      shot_type = PlayerStat.determine_shot_type(3)
-      expect(shot_type).to eq("great_shot_percentage")
-      shot_type = PlayerStat.determine_shot_type(4)
-      expect(shot_type).to eq("push_percentage")
-      shot_type = PlayerStat.determine_shot_type(5)
-      expect(shot_type).to eq("slice_percentage")
-      shot_type = PlayerStat.determine_shot_type(6)
-      expect(shot_type).to eq("duff_percentage")
+    before(:each) do
+      @player = Player.create(id: 100, name: "Happy Gilmore", email: "happy@gmail.com", password: "password")
+      @player_2 = Player.create(name: "Grizzly Adams", email: "HadABeard@gmail.com", password: "cool")
+      @club = Club.create(style_of_club: 1)
+      @club.shots.create(rating: 1, player_id: @player.id)
+      @club.shots.create(rating: 2, player_id: @player.id)
+      @club.shots.create(rating: 3, player_id: @player.id)
+      @club.shots.create(rating: 4, player_id: @player.id)
+      @club.shots.create(rating: 5, player_id: @player.id)
+      @club.shots.create(rating: 6, player_id: @player.id)
+      @hook = PlayerStat.stats(1, @player.id)
+      @pull = PlayerStat.stats(2, @player.id)
+      @great = PlayerStat.stats(3, @player.id)
+      @push = PlayerStat.stats(4, @player.id)
+      @slice = PlayerStat.stats(5, @player.id)
+      @duff = PlayerStat.stats(6, @player.id)
     end
 
     it '#stats' do
-      player = Player.create(name: "Happy Gilmore", email: "happy@gmail.com", password: "password")
-      club = Club.create(style_of_club: 1)
-      club.shots.create(rating: 1, player_id: player.id)
-      club.shots.create(rating: 2, player_id: player.id)
-      club.shots.create(rating: 3, player_id: player.id)
-      club.shots.create(rating: 4, player_id: player.id)
-      club.shots.create(rating: 5, player_id: player.id)
-      club.shots.create(rating: 6, player_id: player.id)
-      hook = PlayerStat.stats(1, player.id)
-      pull = PlayerStat.stats(2, player.id)
-      great = PlayerStat.stats(3, player.id)
-      push = PlayerStat.stats(4, player.id)
-      slice = PlayerStat.stats(5, player.id)
-      duff = PlayerStat.stats(6, player.id)
+      expect(@hook).to eq(17)
+      expect(@pull).to eq(17)
+      expect(@great).to eq(17)
+      expect(@push).to eq(17)
+      expect(@slice).to eq(17)
+      expect(@duff).to eq(17)
+    end
 
-      expect(hook).to eq(17)
-      expect(pull).to eq(17)
-      expect(great).to eq(17)
-      expect(push).to eq(17)
-      expect(slice).to eq(17)
-      expect(duff).to eq(17)
+    it '#update_stats' do
+      previous = PlayerStat.create(player_id: @player.id, hook_percentage: @hook, pull_percentage: @pull, great_shot_percentage: @great, push_percentage: @push, slice_percentage: @slice, duff_percentage: @duff)
+      expect(previous.hook_percentage).to eq(17.0)
+      expect(previous.pull_percentage).to eq(17.0)
+      expect(previous.great_shot_percentage).to eq(17.0)
+      expect(previous.push_percentage).to eq(17.0)
+      expect(previous.slice_percentage).to eq(17.0)
+      expect(previous.duff_percentage).to eq(17.0)
+      @club.shots.create(rating: 1, player_id: @player.id)
+      @club.shots.create(rating: 2, player_id: @player.id)
+
+      updated = @player.player_stats.first
+
+      expect(updated.hook_percentage).to eq(25.0)
+      expect(updated.pull_percentage).to eq(25.0)
+      expect(updated.great_shot_percentage).to eq(13.0)
+      expect(updated.push_percentage).to eq(13.0)
+      expect(updated.slice_percentage).to eq(13.0)
+      expect(updated.duff_percentage).to eq(13.0)
     end
   end
 end
