6a7f2c6ed7526fc1bd54d523d374c4317f94bf9c
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-08 09:00:16 -0600

    Adds SQL query to return only stats for todays shots.

--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -6,8 +6,7 @@ class PlayerStat < ApplicationRecord
   end
 
   def self.today_stats(rating, player_id)
-    (((Shot.where("shots.player_id = #{player_id} AND shots.rating = #{rating} AND shots.created_at BETWEEN '2019-04-06 00:00:00' AND '2019-04-07 23:59:59'")).count).to_f / (Shot.where(player_id: player_id)).count * 100).round
-    # (((Shot.where("shots.player_id = #{player_id} AND shots.rating = #{rating} AND 'shots.created_at.utc' BETWEEN '#{Time.now.utc.midnight - 1}' AND '#{Time.now.utc.midnight}'")).count).to_f / (Shot.where(player_id: player_id)).count * 100).round
+    (((Shot.where("shots.player_id = #{player_id} AND shots.rating = #{rating} AND shots.created_at >= CURRENT_DATE")).count).to_f / ((Shot.where("shots.player_id = #{player_id} AND shots.created_at >= CURRENT_DATE")).count) * 100).round
   end
 
   def self.update_stats(player_id)
@@ -20,7 +19,7 @@ class PlayerStat < ApplicationRecord
   end
 
   def self.update_today(player_id)
-    
+
     where(player_id: player_id).update({hook_percentage: "#{today_stats(1,player_id)}",
                                         pull_percentage: "#{today_stats(2,player_id)}",
                                         great_shot_percentage: "#{today_stats(3,player_id)}",
